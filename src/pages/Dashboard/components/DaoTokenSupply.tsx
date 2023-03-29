26f6e6da9b1a0c9046c82110bd9f97da37a4335f
ardier16 <ardier16@gmail.com>
2023-03-29 14:13:31 +0300

    Add token to wallet

--- src/pages/Dashboard/components/DaoTokenSupply.tsx
+++ src/pages/Dashboard/components/DaoTokenSupply.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { Button, Dropdown, Icon } from '@q-dev/q-ui-kit';
 import { formatAsset } from '@q-dev/utils';
 import styled from 'styled-components';
 
@@ -7,13 +9,50 @@ import ExplorerAddress from 'components/Custom/ExplorerAddress';
 
 import { useDaoStore } from 'store/dao/hooks';
 
+import { captureError } from 'utils/errors';
 import { fromDecimals } from 'utils/numbers';
 
 const StyledWrapper = styled.div`
-  padding: 24px 16px 16px 24px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  padding: 16px 16px 16px 24px;
+
+  .dao-token-supply__menu {
+    background-color: ${({ theme }) => theme.colors.backgroundPrimary};
+    display: grid;
+    width: max-content;
+    min-width: 156px;
+    padding: 4px 0;
+    border-radius: 8px;
+    border: 1px solid ${({ theme }) => theme.colors.borderSecondary};
+    box-shadow:
+      0 4px 4px ${({ theme }) => theme.colors.blockShadowDark},
+      0 -1px 2px ${({ theme }) => theme.colors.blockShadowLight};
+  }
+
+  .dao-token-supply__menu-btn {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+  }
+
+  .dao-token-supply__menu-item {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    padding: 8px 12px;
+    transition: background-color 100ms ease-out;
+    background-color: transparent;
+    border: none;
+    color: ${({ theme }) => theme.colors.textPrimary};
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.tertiaryLight};
+    }
+  }
+
+  .dao-token-supply__menu-icon {
+    width: 20px;
+    height: auto;
+  }
 
   .dao-token-supply__val {
     margin-top: 4px;
@@ -29,18 +68,69 @@ const StyledWrapper = styled.div`
 function DaoTokenSupply () {
   const { t } = useTranslation();
   const { tokenInfo } = useDaoStore();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  async function addTokenToWallet () {
+    if (!window?.ethereum) return;
+    try {
+      await window.ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20',
+          options: {
+            address: tokenInfo.address,
+            symbol: tokenInfo.symbol,
+            decimals: tokenInfo.decimals,
+          },
+        },
+      });
+    } catch (error) {
+      captureError(error);
+    }
+  }
 
   return (
     <StyledWrapper className="block">
-      <div>
+      <div className="block__header">
         <h2 className="text-lg">{t('DAO_TOKEN_SUPPLY')}</h2>
-        <p className="dao-token-supply__val text-xl font-semibold">
-          {formatAsset(fromDecimals(tokenInfo.totalSupply, tokenInfo.decimals), tokenInfo.symbol)}
-        </p>
-        <div className="dao-token-supply__inactive text-sm">
-          <span className="font-light">{t('CONTRACT')}</span>
-          <ExplorerAddress short address={tokenInfo.address} />
-        </div>
+
+        <Dropdown
+          right
+          open={menuOpen}
+          trigger={(
+            <Button
+              icon
+              className="dao-token-supply__menu-btn"
+              look="ghost"
+              active={menuOpen}
+            >
+              <Icon name="more-vertical" />
+            </Button>
+          )}
+          onToggle={setMenuOpen}
+        >
+          <div className="dao-token-supply__menu">
+            <button
+              className="dao-token-supply__menu-item text-md"
+              onClick={addTokenToWallet}
+            >
+              <img
+                className="dao-token-supply__menu-icon"
+                src="/icons/metamask.svg"
+                alt="metamask"
+              />
+              <span>{t('ADD_TO_WALLET')}</span>
+            </button>
+          </div>
+        </Dropdown>
+      </div>
+
+      <p className="dao-token-supply__val text-xl font-semibold">
+        {formatAsset(fromDecimals(tokenInfo.totalSupply, tokenInfo.decimals), tokenInfo.symbol)}
+      </p>
+      <div className="dao-token-supply__inactive text-sm">
+        <span className="font-light">{t('CONTRACT')}</span>
+        <ExplorerAddress short address={tokenInfo.address} />
       </div>
     </StyledWrapper>
   );
