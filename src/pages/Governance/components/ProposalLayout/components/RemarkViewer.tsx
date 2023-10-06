0ee99302b3dd416589332e84f39f73bf21b68494
Oleh Tryfonov <oleg.himik.trifon@gmail.com>
2023-10-06 14:56:59 +0000

    [QDEV-6997] Fix remark links display

--- src/pages/Governance/components/ProposalLayout/components/RemarkViewer.tsx
+++ src/pages/Governance/components/ProposalLayout/components/RemarkViewer.tsx
@@ -1,12 +1,21 @@
+import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Icon } from '@q-dev/q-ui-kit';
+import escape from 'lodash/escape';
 
 import { formatUrl } from 'utils';
 import { URL_REGEX } from 'utils/validators';
 
 interface Props {
   remark: string;
+}
+
+function parseRemark (remark: string) {
+  const escapedRemark = escape(remark) || '-';
+  const urlRegex = /https?:\/\/(www\.)?[-äöüa-zA-Z0-9@%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-äöüa-zA-Z0-9()@:%_+.~#?&//=]*)/gm;
+
+  return escapedRemark?.replaceAll(urlRegex, match => `<a class="link" href="${match}" target="_blank" rel="noreferrer noopener"><span>${match}</span></a>`);
 }
 
 function RemarkViewer ({ remark }: Props) {
@@ -30,10 +39,13 @@ function RemarkViewer ({ remark }: Props) {
             <p className="text-sm color-secondary">{t('PROPOSAL_LINK_DISCLAIMER')}</p>
           </>
         )
-        : <p className="text-md pre-line">{remark || '–'}</p>
+        : <p
+          dangerouslySetInnerHTML={{ __html: parseRemark(remark) }}
+          className="text-md pre-line"
+        />
       }
     </div>
   );
 }
 
-export default RemarkViewer;
+export default memo(RemarkViewer);
