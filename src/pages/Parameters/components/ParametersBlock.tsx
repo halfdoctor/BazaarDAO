a7c02880fb3e1c320e6c741e401ca3edf6baf62f
Oleh Tryfonov <oleg.himik.trifon@gmail.com>
2023-12-04 15:56:25 +0000

    Simplified view of parameters

--- src/pages/Parameters/components/ParametersBlock.tsx
+++ src/pages/Parameters/components/ParametersBlock.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { media, Spinner, Switch } from '@q-dev/q-ui-kit';
@@ -22,6 +23,16 @@ export const StyledWrapper = styled.div`
     }
   }
 
+  .parameters-block__switch-wrap {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+
+    ${media.lessThan('medium')} {
+      justify-content: flex-start;
+    }
+  }
+
   .parameters-block__loading-wrp {
     display: flex;
     justify-content: center;
@@ -29,51 +40,64 @@ export const StyledWrapper = styled.div`
   }
 `;
 
-interface Props {
+interface GeneralProps {
   title: string;
   parameters: ParameterValue[];
   loading: boolean;
   errorMsg: string;
   emptyMsg?: string;
-  isSwitcherShown?: boolean;
-  switcherValue?: boolean;
-  switcherLabel?: string;
-  onChange?: () => void;
 }
 
-function ParametersBlock ({
-  title,
-  switcherValue,
-  isSwitcherShown,
-  switcherLabel,
-  parameters = [],
-  loading = false,
-  errorMsg = '',
-  onChange
-}: Props) {
+type Props<T = ((val: boolean) => void) | undefined> = T extends undefined
+  ? GeneralProps & { setIsRegularParams?: T }
+  : GeneralProps & { setIsRegularParams: T; isRegularParams: boolean};
+
+function ParametersBlock (props: Props) {
   const { t } = useTranslation();
+  const [isSimplified, setIsSimplified] = useState(true);
 
   return (
     <StyledWrapper className="block">
       <div className="text-h3 parameters-block__title">
-        <h2>{title}</h2>
-        {onChange && isSwitcherShown && switcherLabel &&
-          <Switch
-            label={switcherLabel}
-            value={Boolean(switcherValue)}
-            onChange={onChange}
-          />}
+        <h2>{props.title}</h2>
+        {props.setIsRegularParams
+          ? (<Switch
+            label={t('SHOW_REGULAR_PARAMS')}
+            value={props.isRegularParams}
+            onChange={(val: boolean) => {
+              props.setIsRegularParams(val);
+              setIsSimplified(!val);
+            }}
+          />)
+          : (<Switch
+            label={t('SIMPLIFIED_VIEW')}
+            value={isSimplified}
+            onChange={setIsSimplified}
+          />)
+        }
       </div>
+      {!!props.setIsRegularParams && !props.isRegularParams && (
+        <div className="parameters-block__switch-wrap">
+          <Switch
+            label={t('SIMPLIFIED_VIEW')}
+            value={isSimplified}
+            onChange={setIsSimplified}
+          />
+        </div>
+      )}
 
-      {loading && !parameters.length
+      {props.loading && !props.parameters.length
         ? (
           <div className="parameters-block__loading-wrp">
             <Spinner size={32} />
           </div>
         )
-        : errorMsg || !parameters.length
-          ? <EmptyListViewer message={errorMsg || t('NO_PARAMETERS')} />
-          : <ParametersTable parameters={parameters} />
+        : props.errorMsg || !props.parameters.length
+          ? <EmptyListViewer message={props.errorMsg || t('NO_PARAMETERS')} />
+          : <ParametersTable
+            parameters={props.parameters}
+            simplified={isSimplified}
+          />
       }
     </StyledWrapper>
   );
