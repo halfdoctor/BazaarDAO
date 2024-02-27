7255c1ea73d61d3f89bee85b683d17f63e6768a9
halfdoctor <nmanura@gmail.com>
2024-02-27 19:58:34 +0000

    Update SelectDAO.tsx
    Added BazaarX to select list

--- src/pages/SelectDAO/SelectDAO.tsx
+++ src/pages/SelectDAO/SelectDAO.tsx
@@ -13,6 +13,13 @@ function SelectDao () {
       title={t('DAOS_TITLE')}
     >
       <DaoAddressForm />
+<a
+        href="/0x6c9269258c8518E9F85409C5fF93193413a9E451/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        The Bazaar
+      </a>
       {/* TODO: to activate `DaosList` when the real DAOs show up. */}
       {/* <DaosList /> */}
     </PageLayout>
